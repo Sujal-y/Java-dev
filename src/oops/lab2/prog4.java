11d8d4ea9f3993ea6514ec318e114c54707a4c83
Sujal-y <sujalyasho2006@gmail.com>
2025-01-29 22:38:37 +0530

    lab2

--- src/oops/lab2/prog4.java
+++ src/oops/lab2/prog4.java
@@ -1,9 +1,23 @@
 package src.oops.lab2;
+import java.util.Scanner;
 //Write a Java program to display non diagonal elements and find their sum.
 //right to the bottom left corner is called non principal diagonal.]
 //        [Hint: Non Principal diagonal: The diagonal of a diagonal matrix from the top
 public class prog4 {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("size of nXn array");
+
+        int n = scan.nextInt();
+        int[][] array = new int[n][n];
+        System.out.println("enter");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i][j] = scan.nextInt();
+            }
+
+        }
+
 
     }
 
