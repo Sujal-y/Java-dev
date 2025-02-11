287a2b54d7b41fea6f4fb2e4b52b610cffde2d27
Sujal-y <sujalyasho2006@gmail.com>
2025-02-11 09:24:01 +0530

    oops class

---
+++ src/oops_labs/lab3/additional/prog1.java
@@ -0,0 +1,23 @@
+package src.oops_labs.lab3.additional;
+//Create a class with integer array of size 10 and write methods to perform following. Use
+//Switch case to accept choice from the user.
+//a. Input values into an array
+//b. Display the values
+//c. Display the largest value
+//d. Display the average
+//e. Sort the array in ascending order
+import java.util.Scanner;
+class array_operations{
+    int[] arr =new int[10];
+    void input(){
+        System.out.println("enter 10 elements");
+
+    }
+    void display(){}
+    void largest(){}
+    void average(){}
+    void sort(){}
+}
+public class prog1 {
+
+}
---
+++ src/oops_labs/lab3/additional/prog2.java
@@ -0,0 +1,27 @@
+package src.oops_labs.lab3.additional;
+//The International Standard Book Number (ISBN) is a unique numeric book identifier
+//which is printed on every book. The ISBN is based upon a 10-digit code. The ISBN is
+//legal if:
+//1xdigit1 + 2xdigit2 + 3xdigit3 + 4xdigit4 + 5xdigit5 + 6xdigit6 + 7xdigit7 + 8xdigit8
+//+ 9xdigit9 + 10xdigit10 is divisible by 11.
+//example: For an ISBN 1401601499:
+//Sum=1×1 + 2×4 + 3×0 + 4×1 + 5×6 + 6×0 + 7×1 + 8×4 + 9×9 + 10×9 = 253 which
+//is divisible by 11.
+//Write a program to implement the following methods:
+//inputISBN( ) to read the ISBN code as a 10-digit integer.
+//checkISBN() to perform the following check operations:
+//a. If the ISBN is not a 10-digit integer, output the message “ISBN should be
+//a 10 digit number” and terminate the program.
+//b. If the number is 10-digit, extract the digits of the number and compute the
+//sum as explained above. If the sum is divisible by 11, output the message,
+//“Legal ISBN”; otherwise output the message, “Illegal ISBN”
+class ISBN{
+    void inputISBN(){
+        //read
+    }
+    void checkISBN(){
+        //checks
+    }
+}
+public class prog2 {
+}
