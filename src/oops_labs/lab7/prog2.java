c37406a4090b72016c45f951ac8f974bc111557d
Sujal-y <sujalyasho2006@gmail.com>
2025-04-14 01:06:52 +0530

    Lab 6 prog 1 question added

---
+++ src/oops_labs/lab7/prog2.java
@@ -0,0 +1,12 @@
+package src.oops_labs.lab7;
+//To the already defined EMPLOYEE class, add two subclasses FullTimeEmp and PartTimeEmp and implement the following:
+//i) Include the following data members to the PartTimeEmp class-hoursWorked (int) that represents the number of hours
+// worked by the part-time employee and hourlyRate (double, static and final) that represents the hourly rate at which
+// the part-time employee is paid. Also, override calculateSalary() and displayEmployeeDetails() splayEmployeeDetails()
+// method of the base class to display the part-time employee's details, including the hours worked and hourly rate.
+//73
+//ii) The FullTimeEmployee subclass includes the data members bonus and deductions as additional data members and are
+// of type double and overrides the calculateSalary() and displayEmployeeDetails() methods to incorporate these factors.
+//In main(), create various objects to illustrate the functionality of all the classes
+public class prog2 {
+}
---
+++ src/oops_labs/lab7/prog4.java
@@ -0,0 +1,14 @@
+package src.oops_labs.lab7;
+//Create an Account class that stores customers name, acc-no and type of account.
+// From this derive class current account and savings bank account.
+// Include necessary methods to achieve following tasks
+//i) Accept the deposit from a customer and update the balance
+//ii) Display the balance
+//iii) Compute and deposit interest
+//iv) Permit withdraw and update the balance
+//v) Check for minimum balance impose penalty if necessary and update the balance
+//For savings bank account, the facilities provided include computing interest and withdrawal.
+// No interest can be computed on current bank account and a minimum balance must always be maintained.
+// In any instance when it goes below this level, service tax must be imposed.
+public class prog4 {
+}
