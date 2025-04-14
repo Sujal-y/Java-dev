cede994cf9e6e4243856683e2acc42b96d3a85dd
Sujal-y <sujalyasho2006@gmail.com>
2025-04-14 06:06:17 +0530

    oops lab 7 8 9 10

--- src/oops_labs/lab8/prog2.java
+++ src/oops_labs/lab8/prog2.java
@@ -1,5 +1,11 @@
 package src.oops_labs.lab8;
-
+//Design an interface called Series with the following methods
+//i) getNext (returns the next number in series)
+//ii) reset(to restart the series)
+//iii) setStart (to set the value from which the series should start)
+//Design a class named ByTwos that implements Series such that it generates a series
+//of numbers, each two greater than the previous one. Also design a class which will
+//include the main method for referencing the interface.
 
 // Series.java
 interface Series {
